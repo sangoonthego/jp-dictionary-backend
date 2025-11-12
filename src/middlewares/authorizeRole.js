d047b98b263d7ee0382fc6a433323c7332c513a0
Nguyen Ngoc <knkidngoc@gmail.com>
2025-11-12 15:47:26 +0700

    add errorHandle and authorize role

---
+++ src/middlewares/authorizeRole.js
@@ -0,0 +1,12 @@
+const authorizeRole = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user) return res.status(401).json({ message: "Unauthorized" });
+
+        if (!roles.includes(req.user.role))
+            return res.status(403).json({ message: "Forbidden: Access Denied!!!" });
+
+        next();
+    }
+}
+
+module.exports = authorizeRole;
